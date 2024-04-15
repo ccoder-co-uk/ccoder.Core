1e75f91fcd4dd7157342270bbded5a45d00461ec
Paul Ward <paul.ward@corporatelinx.com>
2024-04-15 11:56:49 +0100

    CODE RUB: Minor Code Rub

--- src/cCoder.Core.Api/IServiceCollectionExtensions.cs
+++ src/cCoder.Core.Api/IServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using cCoder.Core.Api.Formatters;
-using cCoder.Core.Api.OData;
 using cCoder.Core.Objects;
 using cCoder.Core.Objects.Dtos.Metadata;
 using cCoder.Core.Objects.Entities.CMS;
@@ -18,152 +17,135 @@ using cCoder.Core.Services.Orchestrations;
 using cCoder.Core.Services.Orchestrations.Interfaces;
 using cCoder.Core.Services.Security;
 using cCoder.Core.Services.Workflow;
-using Microsoft.AspNetCore.OData;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-namespace cCoder.Core.Api
+namespace cCoder.Core.Api;
+
+public static partial class IServiceCollectionExtensions
 {
-    public static partial class IServiceCollectionExtensions
+    public static void AddCore(this IServiceCollection services, Action<CoreBuilderOptions> setupAction)
     {
-        public static void AddCore(this IServiceCollection services, Action<CoreBuilderOptions> setupAction)
+        var config = new CoreBuilderOptions(services);
+
+        services.AddScoped<ICoreService<Role>, RoleService>();
+        services.AddScoped<ICoreService<UserRole>, UserRoleService>();
+        services.AddScoped<ICoreService<User>, UserService>();
+
+        setupAction(config);
+    }
+
+    internal static void AddCoreServices(this IServiceCollection services)
+    {
+        // CMS services
+        services.AddScoped<IAppService, AppService>();
+        services.AddScoped<ICoreService<AppCulture>, AppCultureService>();
+        services.AddScoped<ICommonObjectService, CommonObjectService>();
+        services.AddScoped<IComponentService, ComponentService>();
+        services.AddScoped<ICoreService<Content>, ContentService>();
+        services.AddScoped<ICoreService<Culture>, CultureService>();
+        services.AddScoped<ICoreService<Layout>, LayoutService>();
+        services.AddScoped<ICoreService<PackageItem>, PackageItemService>();
+        services.AddScoped<IPackageService, PackageService>();
+        services.AddScoped<IPageService, PageService>();
+        services.AddScoped<ICoreService<PageInfo>, PageInfoService>();
+        services.AddScoped<IResourceService, ResourceService>();
+        services.AddScoped<IScheduledTaskService, ScheduledTaskService>();
+        services.AddScoped<IScriptService, ScriptService>();
+        services.AddScoped<ICoreService<Submission>, SubmissionService>();
+        services.AddScoped<ITemplateService, TemplateService>();
+
+        // Mail services
+        services.AddScoped<IQueuedEmailService, QueuedEmailService>();
+        services.AddScoped<ICoreService<SentEmail>, SentEmailService>();
+
+        // Security services
+        services.AddScoped<ICMSUserRegistrationOrchestrationService, CMSUserRegistrationOrchestrationService>();
+        services.AddScoped<IUserRoleService, UserRoleService>();
+        services.AddScoped<ICoreService<PageRole>, PageRoleService>();
+        services.AddScoped<ICoreService<Privilege>, PrivilegeService>();
+
+        //planning services
+        services.AddScoped<ICoreService<CalendarEvent>, CalendarEventService>();
+        services.AddScoped<ICoreService<Calendar>, CalendarService>();
+        services.AddScoped<ICoreService<BackgroundJob>, BackgroundJobService>();
+
+        // workflow services
+        services.AddScoped<ICoreService<BusinessProcess>, BusinessProcessService>();
+        services.AddScoped<IFlowDefinitionService, FlowDefinitionService>();
+        services.AddScoped<ICoreService<FlowInstanceData>, FlowInstanceDataService>();
+        services.AddScoped<ICoreService<WorkflowEvent>, WorkflowEventService>();
+        
+        // DMS Services.
+        services.AddScoped<ICoreService<FileContent>, FileContentService>();
+        services.AddScoped<ICoreService<FolderRole>, FolderRoleService>();
+    }
+
+    internal static void AddCorePackaging(this IServiceCollection services)
+    {
+        // installers
+        services.AddScoped<IPackageInstaller, CorePackageInstaller>();
+
+        // importers 
+        services.AddScoped<IPackageItemImporter, BusinessProcessImporter>();
+        services.AddScoped<IPackageItemImporter, ComponentImporter>();
+        services.AddScoped<IPackageItemImporter, FlowDefinitionImporter>();
+        services.AddScoped<IPackageItemImporter, FolderRoleImporter>();
+        services.AddScoped<IPackageItemImporter, LayoutImporter>();
+        services.AddScoped<IPackageItemImporter, PageImporter>();
+        services.AddScoped<IPackageItemImporter, PageRoleImporter>();
+        services.AddScoped<IPackageItemImporter, ResourceImporter>();
+        services.AddScoped<IPackageItemImporter, RoleImporter>();
+        services.AddScoped<IPackageItemImporter, ScriptImporter>();
+        services.AddScoped<IPackageItemImporter, TemplateImporter>();
+    }
+
+    internal static void AddCaches(
+        this IServiceCollection services, 
+        IEnumerable<MetadataContainerSet> additionalMetadata)
+    {
+        services.AddSingleton<ICommonObjectCache, CommonObjectCache>();
+
+        services.AddSingleton<IMetadataCache>(ctx => 
+            new MetadataCache(
+                MetadataHelper.MetaForEverything().Union(additionalMetadata), 
+                ctx.GetRequiredService<ICommonObjectCache>()));
+
+        services.AddScoped<IResourceProvider, CoreResourceProvider>();
+    }
+
+    internal static void AddAspNet(this IServiceCollection services)
+    {
+        services.AddRouting();
+        services.AddResponseCompression();
+
+        services.AddHttpClient();
+        services.AddHttpContextAccessor();
+        services.AddScoped(ctx => ctx.GetService<IHttpContextAccessor>()?.HttpContext);
+        services.AddScoped(ctx => ctx.GetService<HttpContext>()?.Request);
+
+        services.AddSession();
+        services.AddHsts(options =>
         {
-            var config = new CoreBuilderOptions(services);
+            options.Preload = true;
+            options.IncludeSubDomains = true;
+            options.MaxAge = TimeSpan.FromMinutes(60);
+        });
 
-            services.AddScoped<ICoreService<Role>, RoleService>();
-            services.AddScoped<ICoreService<UserRole>, UserRoleService>();
-            services.AddScoped<ICoreService<User>, UserService>();
-
-            setupAction(config);
-        }
-
-        internal static void AddCoreServices(this IServiceCollection services)
+        services.AddMvc(options =>
         {
-            // CMS services
-            services.AddScoped<IAppService, AppService>();
-            services.AddScoped<ICoreService<AppCulture>, AppCultureService>();
-            services.AddScoped<ICommonObjectService, CommonObjectService>();
-            services.AddScoped<IComponentService, ComponentService>();
-            services.AddScoped<ICoreService<Content>, ContentService>();
-            services.AddScoped<ICoreService<Culture>, CultureService>();
-            services.AddScoped<ICoreService<Layout>, LayoutService>();
-            services.AddScoped<ICoreService<PackageItem>, PackageItemService>();
-            services.AddScoped<IPackageService, PackageService>();
-            services.AddScoped<IPageService, PageService>();
-            services.AddScoped<ICoreService<PageInfo>, PageInfoService>();
-            services.AddScoped<IResourceService, ResourceService>();
-            services.AddScoped<IScheduledTaskService, ScheduledTaskService>();
-            services.AddScoped<IScriptService, ScriptService>();
-            services.AddScoped<ICoreService<Submission>, SubmissionService>();
-            services.AddScoped<ITemplateService, TemplateService>();
+            options.EnableEndpointRouting = false;
+            options.OutputFormatters.Add(new XmlFormatter());
+            options.OutputFormatters.Add(new CsvFormatter());
+            options.OutputFormatters.Add(new ExcelFormatter());
+        });
+        services.AddRazorPages();
 
-            // Mail services
-            services.AddScoped<IQueuedEmailService, QueuedEmailService>();
-            services.AddScoped<ICoreService<SentEmail>, SentEmailService>();
-
-            // Security services
-            services.AddScoped<ICMSUserRegistrationOrchestrationService, CMSUserRegistrationOrchestrationService>();
-            services.AddScoped<IUserRoleService, UserRoleService>();
-            services.AddScoped<ICoreService<PageRole>, PageRoleService>();
-            services.AddScoped<ICoreService<Privilege>, PrivilegeService>();
-
-            //planning services
-            services.AddScoped<ICoreService<CalendarEvent>, CalendarEventService>();
-            services.AddScoped<ICoreService<Calendar>, CalendarService>();
-            services.AddScoped<ICoreService<BackgroundJob>, BackgroundJobService>();
-
-            // workflow services
-            services.AddScoped<ICoreService<BusinessProcess>, BusinessProcessService>();
-            services.AddScoped<IFlowDefinitionService, FlowDefinitionService>();
-            services.AddScoped<ICoreService<FlowInstanceData>, FlowInstanceDataService>();
-            services.AddScoped<ICoreService<WorkflowEvent>, WorkflowEventService>();
-            
-            // DMS Services.
-            services.AddScoped<ICoreService<FileContent>, FileContentService>();
-            services.AddScoped<ICoreService<FolderRole>, FolderRoleService>();
-        }
-
-        internal static void AddCorePackaging(this IServiceCollection services)
+        services.Configure<KestrelServerOptions>(options =>
         {
-            // installers
-            services.AddScoped<IPackageInstaller, CorePackageInstaller>();
+            options.Limits.MaxRequestBodySize = int.MaxValue; // if don't set default value is: 30 MB
+        });
 
-            // importers 
-            services.AddScoped<IPackageItemImporter, BusinessProcessImporter>();
-            services.AddScoped<IPackageItemImporter, ComponentImporter>();
-            services.AddScoped<IPackageItemImporter, FlowDefinitionImporter>();
-            services.AddScoped<IPackageItemImporter, FolderRoleImporter>();
-            services.AddScoped<IPackageItemImporter, LayoutImporter>();
-            services.AddScoped<IPackageItemImporter, PageImporter>();
-            services.AddScoped<IPackageItemImporter, PageRoleImporter>();
-            services.AddScoped<IPackageItemImporter, ResourceImporter>();
-            services.AddScoped<IPackageItemImporter, RoleImporter>();
-            services.AddScoped<IPackageItemImporter, ScriptImporter>();
-            services.AddScoped<IPackageItemImporter, TemplateImporter>();
-        }
-
-        internal static void AddCaches(
-            this IServiceCollection services, 
-            IEnumerable<MetadataContainerSet> additionalMetadata)
-        {
-            services.AddSingleton<ICommonObjectCache, CommonObjectCache>();
-
-            services.AddSingleton<IMetadataCache>(ctx => 
-                new MetadataCache(
-                    MetadataHelper.MetaForEverything().Union(additionalMetadata), 
-                    ctx.GetRequiredService<ICommonObjectCache>()));
-
-            services.AddScoped<IResourceProvider, CoreResourceProvider>();
-        }
-
-        internal static void AddCoreApi(this IServiceCollection services)
-        {
-            services.AddAspNet();
-
-            //TODO break this out in to sub models
-            services.AddControllers()
-                .AddOData(opt =>
-                {
-                    opt.RouteOptions.EnableQualifiedOperationCall = false;
-                    opt.EnableAttributeRouting = true;
-                    _ = opt.Expand().Count().Filter().Select().OrderBy().SetMaxTop(1000);
-                    opt.AddRouteComponents($"/Api/cCoder.Core", new CoreModelBuilder().Build().EDMModel);
-                });
-        }
-
-        static void AddAspNet(this IServiceCollection services)
-        {
-            services.AddRouting();
-            services.AddResponseCompression();
-
-            services.AddHttpClient();
-            services.AddHttpContextAccessor();
-            services.AddScoped(ctx => ctx.GetService<IHttpContextAccessor>()?.HttpContext);
-            services.AddScoped(ctx => ctx.GetService<HttpContext>()?.Request);
-
-            services.AddSession();
-            services.AddHsts(options =>
-            {
-                options.Preload = true;
-                options.IncludeSubDomains = true;
-                options.MaxAge = TimeSpan.FromMinutes(60);
-            });
-
-            services.AddMvc(options =>
-            {
-                options.EnableEndpointRouting = false;
-                options.OutputFormatters.Add(new XmlFormatter());
-                options.OutputFormatters.Add(new CsvFormatter());
-                options.OutputFormatters.Add(new ExcelFormatter());
-            });
-            services.AddRazorPages();
-
-            services.Configure<KestrelServerOptions>(options =>
-            {
-                options.Limits.MaxRequestBodySize = int.MaxValue; // if don't set default value is: 30 MB
-            });
-
-            services.AddEndpointsApiExplorer();
-            services.AddSignalR();
-        }
+        services.AddEndpointsApiExplorer();
+        services.AddSignalR();
     }
 }
