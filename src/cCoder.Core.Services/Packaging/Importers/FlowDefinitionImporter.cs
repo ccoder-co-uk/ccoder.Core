3e284646ef73b3efa5df55ae7293088c984a3c41
Paul Ward <paul.ward@corporatelinx.com>
2024-11-07 14:00:10 +0000

    MINOR FIX: Retire Business Process entity

--- src/cCoder.Core.Services/Packaging/Importers/FlowDefinitionImporter.cs
+++ src/cCoder.Core.Services/Packaging/Importers/FlowDefinitionImporter.cs
@@ -7,16 +7,13 @@ namespace cCoder.Core.Services.Packaging.Importers;
 
 public class FlowDefinitionImporter : CoreImporter<FlowDefinition>
 {
-    private readonly ICoreService<BusinessProcess> processService;
     private readonly ILogger log;
 
     public FlowDefinitionImporter(
         IFlowDefinitionService service,
-        ICoreService<BusinessProcess> processService,
         ILogger<FlowDefinitionImporter> log)
             : base(service, "Core/FlowDefinition")
-    {
-        this.processService = processService;
+    { 
         this.log = log;
     }
 
@@ -24,16 +21,15 @@ public class FlowDefinitionImporter : CoreImporter<FlowDefinition>
     {
         //
         //ProcessName property on the dynamic export...
-        dynamic[] dynamicSet = item.Data.StartsWith("{") ? new[] { item.Unpack<dynamic>() } : item.Unpack<dynamic[]>();
-        FlowDefinition[] items = item.Data.StartsWith("{") ? new[] { item.Unpack<FlowDefinition>() } : item.Unpack<FlowDefinition[]>();
+        dynamic[] dynamicSet = item.Data.StartsWith('{') ? [item.Unpack<dynamic>()] : item.Unpack<dynamic[]>();
+        FlowDefinition[] items = item.Data.StartsWith('{') ? [item.Unpack<FlowDefinition>()] : item.Unpack<FlowDefinition[]>();
 
         string[] names = items.Select(l => l.Name.ToLower()).ToArray();
-        IQueryable<BusinessProcess> processes = processService.GetAll().Where(f => f.AppId == appId);
 
         var dbVersions = Service.GetAll(false)
             .AsQueryable()
             .Where(c => c.AppId == appId && names.Contains(c.Name.ToLower()))
-            .Select(l => new { l.Id, l.Name, ProcessName = l.Process.Name })
+            .Select(l => new { l.Id, l.Name, ProcessName = l.App.Name })
             .ToArray();
 
         log.LogDebug($"Existing Flow Definition Items:\n{dbVersions.ToJsonForOdata()}");
@@ -44,7 +40,6 @@ public class FlowDefinitionImporter : CoreImporter<FlowDefinition>
             dynamic dynamicFlow = dynamicSet[i];
             string processName = (string)dynamicFlow.ProcessName;
             var dbFlow = dbVersions.FirstOrDefault(j => dynamicSet[i].ProcessName == j.ProcessName && j.Name.ToLower() == flow.Name.ToLower());
-            flow.ProcessId = processes.FirstOrDefault(p => p.Name == processName)?.Id;
             flow.AppId = appId;
             flow.Id = dbFlow?.Id ?? Guid.Empty;
         }
