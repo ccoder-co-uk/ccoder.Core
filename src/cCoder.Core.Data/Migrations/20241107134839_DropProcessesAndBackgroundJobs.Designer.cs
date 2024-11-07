90223be8a50e1674d1e7e4d6d2341a2654d7d8d3
Paul Ward <paul.ward@corporatelinx.com>
2024-11-07 14:01:03 +0000

    MINOR FIX: Apply changes to API and DB models, remove from App, and deregister removed services

--- src/cCoder.Core.Data/Migrations/20241107134839_DropProcessesAndBackgroundJobs.Designer.cs
+++ src/cCoder.Core.Data/Migrations/20241107134839_DropProcessesAndBackgroundJobs.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using cCoder.Core.Data;
 
@@ -11,9 +12,11 @@ using cCoder.Core.Data;
 namespace cCoder.Core.Migrations
 {
     [DbContext(typeof(CoreDataContext))]
-    partial class CoreDataContextModelSnapshot : ModelSnapshot
+    [Migration("20241107134839_DropProcessesAndBackgroundJobs")]
+    partial class DropProcessesAndBackgroundJobs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
