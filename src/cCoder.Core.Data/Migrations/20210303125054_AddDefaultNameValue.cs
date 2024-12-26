3fcb3fb26867781e2c2c6601402aec00f97f361f
Callum Marshall <callummarshall9@googlemail.com>
2024-12-26 16:30:01 +0000

    HOTFIX: Fix migration hard coding database name

--- src/cCoder.Core.Data/Migrations/20210303125054_AddDefaultNameValue.cs
+++ src/cCoder.Core.Data/Migrations/20210303125054_AddDefaultNameValue.cs
@@ -4,7 +4,7 @@ namespace cCoder.Core.Migrations;
 
 public partial class AddDefaultNameValue : Migration
 {
-    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.Sql(@"UPDATE Per SET Per.[Name]=(SELECT Title FROM [CMS].[PageInfo] pii WHERE pii.CultureId='' AND pii.PageId=Per.Id) FROM [cCoder.Core].[CMS].[Pages] Per WHERE LEN(Per.[Name]) = 0");
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.Sql(@"UPDATE Per SET Per.[Name]=(SELECT Title FROM [CMS].[PageInfo] pii WHERE pii.CultureId='' AND pii.PageId=Per.Id) FROM [CMS].[Pages] Per WHERE LEN(Per.[Name]) = 0");
 
     protected override void Down(MigrationBuilder migrationBuilder)
     {
