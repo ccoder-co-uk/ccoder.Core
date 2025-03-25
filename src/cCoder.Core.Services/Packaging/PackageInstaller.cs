067d1b3e0200152377d9f5480f25cb3bd25e79bd
Paul Ward <paul.ward@ccoder.co.uk>
2025-03-25 12:19:58 +0000

    Log migrated item imports

--- src/cCoder.Core.Services/Packaging/PackageInstaller.cs
+++ src/cCoder.Core.Services/Packaging/PackageInstaller.cs
@@ -1,27 +1,36 @@
 ﻿using cCoder.Core.Objects;
 using cCoder.Core.Objects.Entities.Packaging;
+using Microsoft.Extensions.Logging;
 using System.Security;
 
 namespace cCoder.Core.Services.Packaging;
 
 public abstract class PackageInstaller : IPackageInstaller
 {
+    private readonly ILogger log;
+
     public string Type { get; set; }
     protected ICoreDataContext Core { get; private set; }
     protected IEnumerable<IPackageItemImporter> Importers { get; set; }
 
-    protected PackageInstaller(ICoreDataContext db, IEnumerable<IPackageItemImporter> importers)
+    protected PackageInstaller(ILogger log, ICoreDataContext db, IEnumerable<IPackageItemImporter> importers)
     {
+        this.log = log;
         Core = db;
         Importers = importers;
     }
 
     public virtual async Task Import(int appId, Package package)
     {
+        if (package.Items is null)
+            return;
+
         if (Core.User.IsAdminOfApp(appId))
-            foreach (PackageItem item in package?.Items)
+            foreach (PackageItem item in package.Items)
             {
                 IPackageItemImporter[] importers = Importers.Where(i => i.Type == item.Type).ToArray();
+                log.LogDebug("Importing {ItemType} from {PackageSource}", item.Type, package.SourceApi);
+
                 foreach (IPackageItemImporter importer in importers)
                     await importer.Import(appId, item);
             }
