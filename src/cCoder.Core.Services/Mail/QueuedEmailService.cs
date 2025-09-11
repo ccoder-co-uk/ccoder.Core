5a7ea41a9b75a4188b0d5ebd1039ddf1e96d895b
ashley.upson <ashley.upson@corporatelinx.co.uk>
2025-09-11 10:40:40 +0100

    Minor fix: update QueuedEmailService to allow sending emails as an unauthenticated user

--- src/cCoder.Core.Services/Mail/QueuedEmailService.cs
+++ src/cCoder.Core.Services/Mail/QueuedEmailService.cs
@@ -1,5 +1,9 @@
 ﻿using cCoder.Core.Objects;
+using cCoder.Core.Objects.Dtos;
+using cCoder.Core.Objects.Dtos.Mail;
+using cCoder.Core.Objects.Entities.CMS;
 using cCoder.Core.Objects.Entities.Mail;
+using cCoder.Core.Objects.Entities.Security;
 using Microsoft.EntityFrameworkCore;
 using System.Security;
 
@@ -33,5 +37,62 @@ public class QueuedEmailService : CoreService<QueuedEmail>, IQueuedEmailService
 
         await Db.DeleteAllAsync(queuedEmail.FailedSends);
         await Db.DeleteAsync(queuedEmail);
+    }
+
+    public async Task<QueuedEmail> AddTemplatedEmailAsync(TemplatedEmailDetails details, User coreUser)
+    {
+        App app = GetAppForTemplatedEmail(details.SourceDomain, details.TemplateName);
+
+        Template template = app.Templates
+            .FirstOrDefault(t => t.AppId == app.Id && t.Name == details.TemplateName);
+
+        MailServer mailServer = app.MailServers
+            .FirstOrDefault(s => s.Name == "Default") ??
+                app.MailServers.FirstOrDefault();
+
+        var renderModel = new
+        {
+            Data = details.Model,
+            CoreUser = coreUser
+        };
+
+        renderModel.CoreUser.DefaultCulture = new Culture { Id = details.Culture ?? app.DefaultCultureId };
+
+        var renderParams = new TemplateRenderParams(
+            app,
+            renderModel.CoreUser,
+            renderModel.CoreUser.DefaultCultureId);
+
+        QueuedEmail email = template.BuildEmailTo(
+                details.ToEmail,
+                $"{app.Name}: {details.Subject}",
+                renderParams,
+                renderModel,
+                mailServer);
+
+        email.SentByUserId = renderModel.CoreUser?.Id;
+
+        return await AddAsync(
+            email,
+            checkPrivs: false);
+    }
+
+    private App GetAppForTemplatedEmail(string sourceDomain, string templateName)
+    {
+        App app = Db.GetAll<App>(false)
+            .Include(a => a.Templates)
+            .Include(s => s.MailServers)
+            .FirstOrDefault(a => a.Domain == sourceDomain);
+
+        if (app == null)
+            throw new Exception($"No app found for domain '{sourceDomain}'");
+
+        if (!app.Templates.Any(t => t.Name == templateName))
+            throw new Exception($"No template named '{templateName}' found for domain '{sourceDomain}'");
+
+        if (!app.MailServers.Any())
+            throw new Exception($"No mail server found for domain '{sourceDomain}'");
+
+        return app;
     }
 }
