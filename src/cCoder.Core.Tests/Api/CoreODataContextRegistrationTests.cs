fe0e14b49018ad8657860636c9852dc365723645
Paul Ward <paul.ward@ccoder.co.uk>
2026-07-31 20:44:51 +0100

    Stop advertising Core as an OData context

---
+++ src/cCoder.Core.Tests/Api/CoreODataContextRegistrationTests.cs
@@ -0,0 +1,71 @@
+// ---------------------------------------------------------------
+// Copyright (c) Paul.Ward@ccoder.co.uk
+// ---------------------------------------------------------------
+
+using cCoder.Core.Models;
+using cCoder.Data.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.OData;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OData.Edm;
+using Xunit;
+
+namespace cCoder.Core.Tests.Api;
+
+public sealed partial class CoreODataContextRegistrationTests
+{
+    [Fact]
+    public void ShouldNotAdvertiseCoreAsAnODataContext()
+    {
+        // Given
+        ServiceCollection services = new();
+
+        // When
+        services.AddCoreApiContexts();
+
+        // Then
+        services.BuildServiceProvider(
+                options: new ServiceProviderOptions())
+            .GetServices<ApiInfo>()
+            .Should()
+            .NotContain(predicate: info =>
+                string.Equals(
+                    a: info.Name,
+                    b: "Core",
+                    comparisonType: StringComparison.OrdinalIgnoreCase));
+    }
+
+    [Fact]
+    public void ShouldNotRegisterCoreAsAnODataRouteComponent()
+    {
+        // Given
+        ServiceCollection services = new();
+        EdmModel model = new();
+        services.AddLogging();
+
+        CoreApiRouteDefinition[] routeDefinitions =
+        [
+            new(Name: "Core", RoutePath: "Api/Core", RouteModel: model),
+            new(Name: "Security", RoutePath: "Api/Security", RouteModel: model),
+        ];
+
+        // When
+        services.AddCoreODataExposures(
+            routeDefinitions: routeDefinitions);
+
+        // Then
+        ODataOptions options = services.BuildServiceProvider(
+                options: new ServiceProviderOptions())
+            .GetRequiredService<IOptions<ODataOptions>>()
+            .Value;
+
+        options.RouteComponents
+            .Should()
+            .ContainKey(expected: "Api/Security");
+
+        options.RouteComponents
+            .Should()
+            .NotContainKey(unexpected: "Api/Core");
+    }
+}
