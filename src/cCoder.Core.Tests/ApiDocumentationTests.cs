fd73afa11b1f137e21671305a7e3e8d19f6288d5
Paul Ward <paul.ward@ccoder.co.uk>
2026-07-31 20:44:58 +0100

    Remove legacy v1 Swagger alias

---
+++ src/cCoder.Core.Tests/ApiDocumentationTests.cs
@@ -0,0 +1,99 @@
+// ---------------------------------------------------------------
+// Copyright (c) Paul.Ward@ccoder.co.uk
+// ---------------------------------------------------------------
+
+using cCoder.Core.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Moq;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Xunit;
+
+namespace cCoder.Core.Tests;
+
+public sealed partial class ApiDocumentationTests
+{
+    [Fact]
+    public void ShouldRegisterNamedSwaggerDocumentsWithoutLegacyV1Alias()
+    {
+        // Given
+        ServiceCollection services = new();
+
+        services.AddSingleton<IWebHostEnvironment>(
+            implementationInstance: Mock.Of<IWebHostEnvironment>());
+
+        CoreApiRouteDefinition[] routes =
+        [
+            new(Name: "Core", RoutePath: "Api/Core", RouteModel: null),
+            new(Name: "ContentManagement", RoutePath: "Api/ContentManagement", RouteModel: null),
+        ];
+
+        // When
+        services.AddCoreApiDocumentation(routes: routes);
+
+        using ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+        SwaggerGeneratorOptions options = serviceProvider
+            .GetRequiredService<IOptions<SwaggerGeneratorOptions>>()
+            .Value;
+
+        // Then
+        options.SwaggerDocs.Keys.Should()
+            .BeEquivalentTo(expectation: ["Core", "ContentManagement"]);
+
+        options.SwaggerDocs.Keys.Should()
+            .NotContain(unexpected: "v1");
+    }
+
+    [Fact]
+    public void ShouldPartitionOperationsIntoNamedSwaggerDocuments()
+    {
+        // Given
+        ServiceCollection services = new();
+
+        services.AddSingleton<IWebHostEnvironment>(
+            implementationInstance: Mock.Of<IWebHostEnvironment>());
+
+        CoreApiRouteDefinition[] routes =
+        [
+            new(Name: "Core", RoutePath: "Api/Core", RouteModel: null),
+            new(Name: "ContentManagement", RoutePath: "Api/ContentManagement", RouteModel: null),
+        ];
+
+        services.AddCoreApiDocumentation(routes: routes);
+
+        using ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+        SwaggerGeneratorOptions options = serviceProvider
+            .GetRequiredService<IOptions<SwaggerGeneratorOptions>>()
+            .Value;
+
+        // When
+        bool coreIncludesCoreOperation = options.DocInclusionPredicate.Invoke(
+            arg1: "Core",
+            arg2: new() { RelativePath = "Api/Core/App" });
+
+        bool coreIncludesContentManagementOperation = options.DocInclusionPredicate.Invoke(
+            arg1: "Core",
+            arg2: new() { RelativePath = "Api/ContentManagement/Page" });
+
+        bool contentManagementIncludesOperation = options.DocInclusionPredicate.Invoke(
+            arg1: "ContentManagement",
+            arg2: new() { RelativePath = "Api/ContentManagement/Page" });
+
+        // Then
+        coreIncludesCoreOperation
+            .Should()
+            .BeTrue();
+
+        coreIncludesContentManagementOperation
+            .Should()
+            .BeFalse();
+
+        contentManagementIncludesOperation
+            .Should()
+            .BeTrue();
+    }
+}
