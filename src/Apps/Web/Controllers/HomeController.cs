6896564348cd7925eefb6bf2a9d3822350171f60
Paul Ward <paul.ward@ccoder.co.uk>
2026-08-01 13:09:33 +0100

    Classify page validation outcomes for deployment builds

--- src/Apps/Web/Controllers/HomeController.cs
+++ src/Apps/Web/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ using Web.Dependencies.Filters;
 using Web.Exposures;
 using App = cCoder.Data.Models.CMS.App;
 using RenderResult = cCoder.ContentManagement.Models.RenderResult;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace Web.Controllers
@@ -134,12 +135,16 @@ request:                     new PageRenderRequest
                 viewResult.StatusCode = page.StatusCode;
                 return viewResult;
             }
+            catch (ValidationException)
+            {
+                return BadRequest(error: "The page request is invalid.");
+            }
             catch (Exception exception) when (
                 exception.GetType().Name.Contains(
                     value: "Validation",
                     comparisonType: StringComparison.OrdinalIgnoreCase))
             {
-                return BadRequest(error: "The page request is invalid.");
+                throw;
             }
             catch (Exception)
             {
